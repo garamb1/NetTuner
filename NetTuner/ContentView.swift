9bf5c70f9e7a7a88aba457b6297f44105170c97e
Vincenzo Garambone <garambo@gmail.com>
2024-04-15 11:46:37 +0200

    Initial Commit

---
+++ NetTuner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetTuner
+//
+//  Created by Vincenzo Garambone on 15/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NetTuner/NetTunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetTunerApp.swift
+//  NetTuner
+//
+//  Created by Vincenzo Garambone on 15/04/24.
+//
+
+import SwiftUI
+
+@main
+struct NetTunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
